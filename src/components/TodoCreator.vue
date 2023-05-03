225ad0e8fa8bd94c02abea2f50df5e8f0c947b4e
Alberto Ortiz <65422300+tizor98@users.noreply.github.com>
2023-05-02 23:10:21 -0500

    first commit

---
+++ src/components/TodoCreator.vue
@@ -0,0 +1,47 @@
+<template>
+   <div class="input-wrap">
+      <input type="text" v-model="todo" />
+      <button @click="storeTodo()" >Create</button>
+   </div>
+</template>
+
+<script setup lang="ts">
+import { ref, type Ref } from 'vue';
+
+const todo: Ref<string> = ref('');
+
+const emit = defineEmits(['create-todo']);
+
+const storeTodo = () => {
+   emit('create-todo', todo.value)
+}
+
+</script>
+
+<style lang="scss" scoped>
+   .input-wrap {
+      display: flex;
+      transition: 250ms ease;
+      border: 2px solid #41b080;
+
+      &:focus-within {
+         box-shadow: 0 -4px 6px -1px rgb(0 0 0 / 0.1),
+            0 -2px 4px -2px rgb(0 0 0 / 0.1);
+      }
+
+      input {
+         width: 100%;
+         padding: 8px 6px;
+         border: none;
+
+         &:focus {
+            outline: none;
+         }
+      }
+
+      button {
+         padding: 8px 16px;
+         border: none;
+      }
+   }
+</style>
---
+++ src/views/TodoView.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import TodoCreator from '@/components/TodoCreator.vue';
+import { ref, type Ref } from 'vue';
+import { uid } from 'uid';
+
+type Todo = {
+   id: string,
+   todo: string,
+   isCompleted: boolean,
+   isEditing: boolean,
+}
+
+const todoList: Ref<Todo[]> = ref([]);
+
+const createTodo = (todo: string) => {
+   todoList.value.push({
+      id: uid(),
+      todo,
+      isCompleted: false,
+      isEditing: false,
+   })
+}
+
+</script>
+
+<template>
+   <main>
+      <h1>Create ToDo</h1>
+      <TodoCreator @create-todo="createTodo" />
+   </main>
+</template>
+
+<style lang="scss" scoped>
+   main {
+      display: flex;
+      flex-direction: column;
+      max-width: 500px;
+      width: 100%;
+      margin: 0 auto;
+      padding: 40px 16px;
+
+      h1 {
+         margin-bottom: 16px;
+         text-align: center;
+      }
+   }
+</style>
